177bcd482f3b857b88712135194e9d6f439ca7f8
Szőllősi-Maruzs Eszter <k71608883429@ktch.hu>
2023-01-17 19:09:51 +0100

    Az index.html és az app.js kapott egy fejlécet

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+/*
+File: index.html
+Author: Szőllősi-Maruzs Eszter
+Copyright: 2023, Szőllősi-Maruzs Eszter
+Group: Szoft I/1-E
+Date: 2023-01-17
+Github: https://github.com/wfpbmaresz/Haromszog
+Licenc: GNU GPL
+*/
 const baseElem = document.querySelector("#base");
 const heightElem = document.querySelector("#height");
 const calcButton = document.querySelector("#calcButton");
